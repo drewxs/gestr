a52bb28dcec58da05400f7320d147eb464e88626
Andrew X. Shah <drewshah0@gmail.com>
2023-01-24 23:13:21 -0700

    feat: add basic gesture recognition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ import { Inter } from '@next/font/google';
 import * as model from '@tensorflow-models/handpose';
 import * as _tf from '@tensorflow/tfjs';
 import '@tensorflow/tfjs-backend-webgl';
-import { useRef } from 'react';
+import * as fp from 'fingerpose';
+import { useRef, useState } from 'react';
 import Webcam from 'react-webcam';
 
 import { drawHand } from '@/utils';
@@ -14,13 +15,13 @@ import styles from './page.module.css';
 const inter = Inter({ subsets: ['latin'] });
 
 const Home = () => {
+  const [gesture, setGesture] = useState<string | null>(null);
+
   const webcamRef = useRef<Webcam>(null!);
   const canvasRef = useRef<HTMLCanvasElement>(null!);
 
   const load = async () => {
     const net: model.HandPose = await model.load();
-    console.log('model loaded');
-
     setInterval(() => {
       detect(net);
     }, 50);
@@ -40,6 +41,27 @@ const Home = () => {
 
       const hand = await net.estimateHands(video);
 
+      if (hand.length > 0) {
+        const GE = new fp.GestureEstimator([
+          fp.Gestures.VictoryGesture,
+          fp.Gestures.ThumbsUpGesture,
+        ]);
+
+        const gesture = await GE.estimate(hand[0].landmarks, 8);
+
+        if (gesture.gestures?.length > 0) {
+          const confidence = gesture.gestures.map(
+            (prediction: { score: number }) => prediction.score,
+          );
+          const maxConfidence = confidence.indexOf(Math.max.apply(null, confidence));
+          setGesture(gesture.gestures[maxConfidence].name);
+        } else {
+          setGesture('');
+        }
+      } else {
+        setGesture('');
+      }
+
       const ctx = canvasRef.current.getContext('2d')!;
       drawHand(hand, ctx);
     }
@@ -48,9 +70,10 @@ const Home = () => {
   load();
 
   return (
-    <main className={inter.className}>
+    <main className={[inter.className, styles['root']].join(' ')}>
       <Webcam ref={webcamRef} className={styles['webcam']} />
       <canvas ref={canvasRef} className={styles['canvas']} />
+      <h1 className={styles['gesture']}>{gesture}</h1>
     </main>
   );
 };
