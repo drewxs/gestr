9c5c107cfc94c1772ebf4bf4b9b43ed3790984f8
Andrew X. Shah <drewshah0@gmail.com>
2023-01-27 21:34:34 -0700

    refactor: joints to matrix

--- src/utils/canvas.ts
+++ src/utils/canvas.ts
@@ -4,27 +4,23 @@ interface Prediction {
   landmarks: Coordinate[];
 }
 
-interface FingerJoints {
-  [key: string]: [number, number, number, number, number];
-}
-
-const fingerJoints: FingerJoints = {
-  thumb: [0, 1, 2, 3, 4],
-  indexFinger: [0, 5, 6, 7, 8],
-  middleFinger: [0, 9, 10, 11, 12],
-  ringFinger: [0, 13, 14, 15, 16],
-  pinky: [0, 17, 18, 19, 20],
-};
+const fingerJoints: number[][] = [
+  [0, 1, 2, 3, 4], // thumb
+  [0, 5, 6, 7, 8], // index finger
+  [0, 9, 10, 11, 12], // middle finger
+  [0, 13, 14, 15, 16], // ring finger
+  [0, 17, 18, 19, 20], // pinky
+];
 
 export const drawHand = (predictions: Prediction[], ctx: CanvasRenderingContext2D) => {
   if (predictions.length > 0) {
     predictions.forEach((prediction) => {
       const landmarks = prediction.landmarks;
 
-      for (const finger in fingerJoints) {
-        for (let j = 0; j < fingerJoints[finger].length - 1; j++) {
-          const firstJointIndex = fingerJoints[finger][j];
-          const secondJointIndex = fingerJoints[finger][j + 1];
+      for (let i = 0; i < fingerJoints.length; i++) {
+        for (let j = 0; j < fingerJoints[i].length - 1; j++) {
+          const firstJointIndex = fingerJoints[i][j];
+          const secondJointIndex = fingerJoints[i][j + 1];
 
           ctx.beginPath();
           ctx.moveTo(landmarks[firstJointIndex][0], landmarks[firstJointIndex][1]);
@@ -43,7 +39,7 @@ export const drawHand = (predictions: Prediction[], ctx: CanvasRenderingContext2
         ctx.beginPath();
         ctx.arc(x, y, 3, 0, 3 * Math.PI);
 
-        ctx.fillStyle = 'cyan';
+        ctx.fillStyle = '#00ff00';
         ctx.fill();
       }
     });
